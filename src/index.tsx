ab1a56497dcf814b55f976fcd5432b132079118c
Picculus254 <joelb.p.dasilva@gmail.com>
2025-07-31 20:19:49 +0100

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error('O elemento com id "root" não foi encontrado no HTML.');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
