cd864ec7c964e540b51bf123e5533b0106de4e11
Picculus254 <joelb.p.dasilva@gmail.com>
2025-07-31 20:23:22 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <h1>Cafè Please</h1>
+      <p>App carregada com sucesso!</p>
+    </main>
+  );
+};
+
+export default App;
